6a181d3b8feb7dde767a4bbb98e6c897b82b1292
100kgtrotila <hirodevi192@gmail.com>
2026-01-23 22:36:10 +0200

    refactor: corrected to more modern style

--- app/core/db.py
+++ app/core/db.py
@@ -1,18 +1,19 @@
+from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 from app.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
-    class_= AsyncSession,
     expire_on_commit=False,
-    autoflush=False
+    autoflush=True
 )
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
-async def get_db():
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
