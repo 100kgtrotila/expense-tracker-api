8b3fcb557694a3568bd654e7ff114bd0ef65f150
100kgtrotila <hirodevi192@gmail.com>
2026-01-23 20:36:29 +0200

    feat: expenses schemas.py

---
+++ app/modules/expenses/schemas.py
@@ -0,0 +1,17 @@
+from decimal import Decimal
+from datetime import datetime
+from pydantic import BaseModel, Field, ConfigDict
+
+class ExpenseBase(BaseModel):
+    category_id: int = Field(..., description="Category ID")
+    name: str = Field(..., min_length=3, max_length=50, description="Expense Name")
+    spent: Decimal = Field(..., gt=0, decimal_places=2, description="Amount spent")
+class CreateExpense(ExpenseBase):
+    pass
+
+class ExpenseResponse(ExpenseBase):
+    id: int
+    user_id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
